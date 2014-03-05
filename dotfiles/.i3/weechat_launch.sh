12d62da8b948b0a46de79a31419c3353dea82248
Josh Chase <jcjoshuachase@gmail.com>
2014-03-05 14:18:11 -0500

    Updated weechat script to use remote server if desired

--- dotfiles/.i3/weechat_launch.sh
+++ dotfiles/.i3/weechat_launch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-[ -z "$(screen -ls | grep irc)" ] && screen -m -d -S irc weechat-curses
+REMOTE=saturn
 
-$HOME/.i3/urxvt_launch.sh -e "screen -d -r irc"
+if [ -z "$REMOTE" ]
+then
+	[ -z "$(screen -ls | grep irc)" ] && screen -m -d -S irc weechat-curses
+
+	$HOME/.i3/urxvt_launch.sh -e "screen -d -r irc"
+else
+	[ -z "$(ssh $REMOTE screen -ls | grep irc)" ] && ssh $REMOTE screen -m -d -S irc weechat-curses
+
+	$HOME/.i3/urxvt_launch.sh -e "ssh $REMOTE -t screen -d -r irc"
+fi
